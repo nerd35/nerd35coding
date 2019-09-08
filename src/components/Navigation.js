1883092f515533bb5e17ac62c4d368e5a4c1e865
Umukoro <umukorochinedu@gmail.com>
2019-09-08 01:19:29 +0100

    navbar mobile turgle

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,16 +6,18 @@ import '../App.css';
 const Navigation = () => {
     return (
 
-            <nav className="navbar navbar-expand-lg navbar-white bg-white text-primary navsec fixed-top mb-4">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white text-primary navsec fixed-top mb-4">
         <div class="container">
           <NavLink className="navbar-brand mt-2 mb-2 ml-5" to="/">
                 <p className="navLogo">NERD<strong class="text-warning logo35">35</strong></p>
               </NavLink>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <button class="navbar-toggler text-warning" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon texy-warning"></span>
+                      </button>
+
+                      <div className="collapse navbar-collapse " id="navbarSupportedContent">
+
             <ul className="navbar-nav ml-auto navlink">
               <li className="nav-item text-black-50 active navlink">
                 <NavLink className="nav-link text-black-50 mr-3 text-center navlink" to="/">Home
